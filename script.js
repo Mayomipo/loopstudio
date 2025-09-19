44ada6568b94d2c15491dbf63951eb67385b2389
Mayomipo Solademi <mayorsolademi@gmail.com>
2025-09-19 13:57:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let hamburger = document.getElementById("hamburger")
+let navcard = document.querySelector('.navcard')
+
+const showNavcard = () => {
+    navcard.classList.add('show')
+}
+const removeNavcard = () => {
+    navcard.classList.remove('show')
+}
